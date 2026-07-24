68d658c56618bf0f2c89289c9cee1cb8dca59db7
Barack Okubi <izobrack3@gmail.com>
2026-07-24 12:52:51 +0300

    Fix cart API duplicate path

--- client/src/api/cartApi.js
+++ client/src/api/cartApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `${import.meta.env.VITE_API_URL}/api/cart`;
+const API = `${import.meta.env.VITE_API_URL}/cart`;
 
 const authConfig = () => ({
   headers: {
