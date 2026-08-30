f6fd137d36d82b6b595aa6073d2e23021f73fd87
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-30 22:34:48 +0300

    feat: add buyer seller directory and storefront routes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,19 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-// Customer pages
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import BookDetails from "../pages/BookDetails";
 import Books from "../pages/Books";
+import Sellers from "../pages/Sellers";
+import SellerStore from "../pages/SellerStore";
 import Cart from "../pages/Cart";
 import Favorites from "../pages/Favorites";
 import Orders from "../pages/Orders";
 import Checkout from "../pages/Checkout";
 
-
-
-// Admin
 import AdminRoute from "./AdminRoute";
 import AdminDashboard from "../pages/AdminDashboard";
 import AdminBooks from "../pages/AdminBooks";
@@ -21,7 +19,6 @@ import AdminOrders from "../pages/AdminOrders";
 import AdminUsers from "../pages/AdminUsers";
 import AdminSellers from "../pages/admin/AdminSellers";
 
-// Seller pages
 import SellerRegister from "../pages/SellerRegister";
 import SellerDashboard from "../pages/seller/SellerDashboard";
 import AddBook from "../pages/seller/AddBook";
@@ -31,30 +28,18 @@ import SellerOrders from "../pages/seller/SellerOrders";
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* Home */}
       <Route path="/" element={<Home />} />
-
-      {/* Authentication */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
-      {/* Books */}
       <Route path="/books" element={<Books />} />
       <Route path="/books/:id" element={<BookDetails />} />
-
-      {/* Cart */}
+      <Route path="/sellers" element={<Sellers />} />
+      <Route path="/sellers/:id" element={<SellerStore />} />
       <Route path="/cart" element={<Cart />} />
-
-      {/* Favorites */}
       <Route path="/favorites" element={<Favorites />} />
-
-      {/* Orders */}
       <Route path="/orders" element={<Orders />} />
-
-      {/* Checkout */}
       <Route path="/checkout" element={<Checkout />} />
 
-      {/* Seller Routes */}
       <Route path="/seller/register" element={<SellerRegister />} />
       <Route path="/seller" element={<SellerDashboard />} />
       <Route path="/seller/dashboard" element={<SellerDashboard />} />
@@ -62,50 +47,11 @@ export default function AppRoutes() {
       <Route path="/seller/books" element={<MyBooks />} />
       <Route path="/seller/orders" element={<SellerOrders />} />
 
-      {/* Admin Routes */}
-      <Route
-        path="/admin/dashboard"
-        element={
-          <AdminRoute>
-            <AdminDashboard />
-          </AdminRoute>
-        }
-      />
-
-      <Route
-        path="/admin/books"
-        element={
-          <AdminRoute>
-            <AdminBooks />
-          </AdminRoute>
-        }
-      />
-
-      <Route
-        path="/admin/orders"
-        element={
-          <AdminRoute>
-            <AdminOrders />
-          </AdminRoute>
-        }
-      />
-
-      <Route
-        path="/admin/users"
-        element={
-          <AdminRoute>
-            <AdminUsers />
-          </AdminRoute>
-        }
-      />
-      <Route
-        path="/admin/sellers"
-        element={
-          <AdminRoute>
-            <AdminSellers />
-          </AdminRoute>
-        }
-      />
+      <Route path="/admin/dashboard" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+      <Route path="/admin/books" element={<AdminRoute><AdminBooks /></AdminRoute>} />
+      <Route path="/admin/orders" element={<AdminRoute><AdminOrders /></AdminRoute>} />
+      <Route path="/admin/users" element={<AdminRoute><AdminUsers /></AdminRoute>} />
+      <Route path="/admin/sellers" element={<AdminRoute><AdminSellers /></AdminRoute>} />
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
