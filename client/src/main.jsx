1e7b0a85aa98ddecb89565a2fc003af3d4baaa30
Barack Okubi <izobrack3@gmail.com>
2026-08-31 00:31:34 +0300

    Add automatic scroll to top on route changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -24,6 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <ScrollToTop />
         <ThemeProvider>
           <AuthProvider>
             <FavoriteProvider>
